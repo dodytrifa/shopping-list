2d920e9fffdf81b0271c4c6a449e70b95a21b1e0
dodytrifa <dody.trifa@gmail.com>
2021-06-25 15:49:14 +0700

    polish project

--- src/App.js
+++ src/App.js
@@ -2,37 +2,89 @@ import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
 
+//buat funct baru terkait LS
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list")
+  if(list) {
+    return JSON.parse(localStorage.getItem("list"))
+  } else {
+    return []
+  }
+}
+
 function App() {
   const [name, setName] = useState('')
-  const [list, setList] = useState([])
+  const [list, setList] = useState(getLocalStorage())
   const [isEditing, setIsEditing] = useState(false)
   const [editId, setEditId] = useState(null)
-  const [alert, setAlert] = useState({show:false, msg:'', type: ''})
+  const [alert, setAlert] = useState({
+    show: false, 
+    msg:'', 
+    type: ''
+  })
 
   const handleSubmit = (e) => {
     e.preventDefault()
     if(!name) {
-
+      showAlert(true, "danger", "please enter value")
     }
     else if(name && isEditing) {
-
+      setList(
+        list.map((item) => {
+          if(item.id === editId) {
+            return {...item, title:name}
+          }
+            return item
+        }))
+      setName('')
+      setEditId(null)
+      setIsEditing(false)
+      showAlert(true, "success", "Item edited")
     }
     else {
+      showAlert(true, "success", "Item successfully added")
       const newItem = {id: new Date().getTime().toString(),title:name};
       setList([...list, newItem]);
       setName('')
     }
   }
 
+  // karena akan dipakai beberapa kali buat fungsi terpisah
+  const showAlert = (show=false, type="", msg="") => {
+    setAlert({show:show, type:type, msg:msg})
+  }
+
+  //ditrigger dari button delete
+  const clearList = () => {
+    showAlert(true, "danger", "empty list")
+    setList([])
+  }
+
+  const removeItem = (id) => {
+    showAlert(true, "danger", "item deleted")
+    setList(list.filter((item)=> item.id !== id))
+  }
+
+  const editItem = (id) => {
+    const specificItem = list.find((item)=> item.id === id)
+    setIsEditing(true)
+    setEditId(id)
+    setName(specificItem.title)
+  }
+
+  //agar list tidak hilang saat refresh, maka set ke LS
+  useEffect(()=> {
+    localStorage.setItem("list", JSON.stringify(list))
+  },[list])
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
       {
-        alert.show && <Alert/>
+        alert.show && <Alert {...alert} removeAlert={showAlert}/>
       }
       <h3>grocery list</h3>
       <div className="form-control">
-
         <input 
         type="text" 
         className="grocery"
@@ -47,8 +99,8 @@ function App() {
       </form>
       {list.length > 0 && 
         <div className='grocery-container'>
-          <List items={list}/>
-          <button className='clear-btn'>
+          <List items={list} removeItem={removeItem} editItem={editItem}/>
+          <button className='clear-btn' onClick={clearList}>
             clear items
           </button>
         </div>
